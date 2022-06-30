2075a7ce40cbfe8628b6c82add13d99090298293
taewoo <rkdxodn6035@gmail.com>
2022-06-30 12:37:18 +0900

    parameter

---
+++ script.groovy
@@ -0,0 +1,27 @@
+
+
+def buildApp() {
+
+	echo 'building the applications...'
+
+}
+
+
+
+def testApp() {
+
+	echo 'testing the applications...'
+
+}
+
+
+
+def deployApp() {
+
+	echo 'deploying the applicaiton...'
+
+	echo "deploying version ${params.VERSION}"
+
+}
+
+return this
